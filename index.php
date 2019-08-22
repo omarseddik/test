05d7fa97ea6c2246f1adbdeffebb99aa70beb63d
Seddik Omar <seddik.omar@gmail.com>
2019-08-22 02:52:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
 phpinfo()
+
+$server = "iis-php.database.windows.net";  //server name   ="welcome"
+$user = "issphp";     //user name     ="std"
+$pass = "sqlserver@1";   // password      ="3434&^%"
+
+
+
+$con = mssql_connect($server, $user, $pass) or die("Couldn't connect to MSSQL Server on $server");
+var_dump($con);
+?>
